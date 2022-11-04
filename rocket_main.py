cd332f2be73e2d0a68a538500a0997de9a0723ef
antoinettehemlall <antoinette.hemlall2@gmail.com>
2022-11-04 12:09:26 -0400

    Refactored screen updates and events.

--- rocket_main.py
+++ rocket_main.py
@@ -17,14 +17,20 @@ class RocketShip:
 
     def run_game(self):
         while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT :
                     sys.exit()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            pygame.display.flip()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     rs = RocketShip()
