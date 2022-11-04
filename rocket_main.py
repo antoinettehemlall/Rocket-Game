f6b40368977190d782400e42473edaae8db997b2
antoinettehemlall <antoinette.hemlall2@gmail.com>
2022-11-04 10:15:58 -0400

    Created settings class

--- rocket_main.py
+++ rocket_main.py
@@ -2,19 +2,24 @@ import sys
 
 import pygame
 
+from settings import Settings
 class RocketShip:
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Rocket Ship", "Try to move the ship!")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Rocket Ship")
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT :
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
