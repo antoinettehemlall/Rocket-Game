5e287b6c94bb42befcddd8b5fc78c285709d39db
antoinettehemlall <antoinette.hemlall2@gmail.com>
2022-11-05 09:59:51 -0400

    Added input for up and down arrow keys

--- rocket_main.py
+++ rocket_main.py
@@ -18,12 +18,32 @@ class RocketShip:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT :
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                    elif event.key == pygame.K_UP:
+                        self.ship.moving_up = True
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = True
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                    elif event.key == pygame.K_UP:
+                        self.ship.moving_up = False
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.moving_down = False
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
--- ship.py
+++ ship.py
@@ -14,6 +14,23 @@ class Ship:
         # Start each new ship at bottom center of screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+        if self.moving_up:
+            self.rect.x += 1
+        if self.moving_down:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw ship at its current location."""
         self.screen.blit(self.image, self.rect)
